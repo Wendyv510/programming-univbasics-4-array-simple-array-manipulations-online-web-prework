1111593bf22be7a9ba55bcaba18031a5c7f1e16a
Wendy Harris <wendyv510@hotmail.com>
2019-09-24 21:07:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,22 @@ def using_push
   colors_in_the_rainbow.push("violet")
 end
 
+def using_unshift
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  @new_neighborhood = "Staten Island"
+  .unshift(bouroughs_in_nyc,@new_neighborhood)
+end
+
+def using_pop
+  @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  @continents.pop("Antarctica")
+  @continents.size 
+end
+
+def pop_with_args
+  @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  @small_dogs = @dog_breeds.pop("Chihuahua", "Shiba Inu")
+end
+
+
+
