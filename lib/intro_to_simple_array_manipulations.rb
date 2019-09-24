8e95b792886fe3430d8f0faa5951b40277c4270b
Wendy Harris <wendyv510@hotmail.com>
2019-09-24 21:17:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,5 +20,21 @@ def pop_with_args
   @small_dogs = @dog_breeds.pop("Chihuahua", "Shiba Inu")
 end
 
+def using_shift 
+  @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  @im_so_over_this_city = .shift(@my_favorite_cities)
+end
 
+def shift_with_args
+  @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+  @ice_cream_brands.shift(2)
+end
 
+def using con_cat
+  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+      more_favs = ["sports cars", "flatiron school"]
+      @all_my_favs =.concat(@my_favorite_things, more_favs)
+    end
+    
+    def using_insert
+      
